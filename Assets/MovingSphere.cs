d64db401ddca919a04343e28b060702d0dffb4d0
Keegan Leary <keegs@Keegans-iMac.local>
2020-05-17 20:15:07 -0700

    First commit

---
+++ Assets/MovingSphere.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+
+    [SerializeField]
+    Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
+    void Update() {
+        Vector3 velocity;
+        Vector2 playerInput;
+
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+        velocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        Vector3 acceleration = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        velocity += acceleration * Time.deltaTime;
+        Vector3 displacement = velocity * Time.deltaTime;
+        
+        Vector3 newPosition = transform.localPosition + displacement;
+        if (!allowedArea.Contains(new Vector2(newPosition.x, newPosition.z))) {
+            newPosition = transform.localPosition;
+        }
+        transform.localPosition = newPosition;
+    }
+
+}
